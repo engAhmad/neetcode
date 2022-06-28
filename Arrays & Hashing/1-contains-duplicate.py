c48ec9d231c396245d87eeb0cda6edefa4070079
ahmad <engAhmad@users.noreply.github.com>
2022-06-28 12:20:08 +0300

    containsDuplicate

---
+++ Arrays & Hashing/1-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        hashset = set()
+        
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+
+nums = [1,2,3,1]
+d = Solution()
+print(d.containsDuplicate(nums))
